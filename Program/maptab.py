ba662bc02fbcb7eb30ca7a4702974b3845ef2ac4
Daniel Ramos <daniel.ramosg@gmail.com>
2015-09-25 00:00:06 +0200

    Fixed coordinate label and widened text column

--- Program/maptab.py
+++ Program/maptab.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'maptab.ui'
 #
-# Created: Sun Sep  6 15:31:41 2015
+# Created: Thu Sep 24 23:52:32 2015
 #      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -26,14 +26,12 @@ except AttributeError:
 class Ui_maptab(object):
     def setupUi(self, maptab):
         maptab.setObjectName(_fromUtf8("maptab"))
-        maptab.resize(946, 553)
+        maptab.resize(1046, 553)
         maptab.setWindowTitle(_fromUtf8("Form"))
         self.horizontalLayout = QtGui.QHBoxLayout(maptab)
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
         self.gridLayout_2 = QtGui.QGridLayout()
         self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
-        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.gridLayout_2.addItem(spacerItem, 1, 0, 1, 1)
         self.map_place = QtGui.QFrame(maptab)
         self.map_place.setMinimumSize(QtCore.QSize(420, 422))
         self.map_place.setFrameShape(QtGui.QFrame.NoFrame)
@@ -45,11 +43,13 @@ class Ui_maptab(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.text_place.sizePolicy().hasHeightForWidth())
         self.text_place.setSizePolicy(sizePolicy)
-        self.text_place.setMinimumSize(QtCore.QSize(500, 0))
+        self.text_place.setMinimumSize(QtCore.QSize(600, 0))
         self.text_place.setFrameShape(QtGui.QFrame.NoFrame)
         self.text_place.setReadOnly(True)
         self.text_place.setObjectName(_fromUtf8("text_place"))
         self.gridLayout_2.addWidget(self.text_place, 0, 2, 2, 1)
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.gridLayout_2.addItem(spacerItem, 1, 0, 1, 1)
         self.gridLayout = QtGui.QGridLayout()
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
         self.label_2 = QtGui.QLabel(maptab)
@@ -57,19 +57,14 @@ class Ui_maptab(object):
         self.label_2.setObjectName(_fromUtf8("label_2"))
         self.gridLayout.addWidget(self.label_2, 0, 0, 1, 1)
         self.radiusbox = QtGui.QDoubleSpinBox(maptab)
-        self.radiusbox.setMaximumSize(QtCore.QSize(70, 16777215))
         self.radiusbox.setProperty("value", 20.0)
         self.radiusbox.setObjectName(_fromUtf8("radiusbox"))
         self.gridLayout.addWidget(self.radiusbox, 0, 1, 1, 1)
         self.clearbutton = QtGui.QPushButton(maptab)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.clearbutton.sizePolicy().hasHeightForWidth())
-        self.clearbutton.setSizePolicy(sizePolicy)
         self.clearbutton.setObjectName(_fromUtf8("clearbutton"))
         self.gridLayout.addWidget(self.clearbutton, 0, 2, 1, 1)
         self.label = QtGui.QLabel(maptab)
+        self.label.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label.setObjectName(_fromUtf8("label"))
         self.gridLayout.addWidget(self.label, 1, 0, 1, 1)
         self.coordlabel = QtGui.QLabel(maptab)
@@ -78,8 +73,6 @@ class Ui_maptab(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.coordlabel.sizePolicy().hasHeightForWidth())
         self.coordlabel.setSizePolicy(sizePolicy)
-        self.coordlabel.setMinimumSize(QtCore.QSize(120, 0))
-        self.coordlabel.setText(_fromUtf8(""))
         self.coordlabel.setObjectName(_fromUtf8("coordlabel"))
         self.gridLayout.addWidget(self.coordlabel, 1, 1, 1, 2)
         self.gridLayout_2.addLayout(self.gridLayout, 1, 1, 1, 1)
@@ -89,7 +82,8 @@ class Ui_maptab(object):
         QtCore.QMetaObject.connectSlotsByName(maptab)
 
     def retranslateUi(self, maptab):
-        self.label_2.setText(_translate("maptab", "r =", None))
+        self.label_2.setText(_translate("maptab", "r=", None))
         self.clearbutton.setText(_translate("maptab", "Clear", None))
         self.label.setText(_translate("maptab", "Coordinates:", None))
+        self.coordlabel.setText(_translate("maptab", " ", None))
 
