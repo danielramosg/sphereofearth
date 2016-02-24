1aa73505486b032e17ed84c5086e15da02ede65e
Daniel Ramos <daniel.ramosg@gmail.com>
2016-02-24 20:52:10 +0100

    New buttons translated.

--- Program/soe_es.ts
+++ Program/soe_es.ts
@@ -1,71 +1,52 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="es_ES" sourcelanguage="en_US">
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES" sourcelanguage="en_US">
 <context>
     <name>Form</name>
     <message>
-        <location filename="ui_soe.py" line="352"/>
+        <location filename="ui_soe.py" line="308"/>
         <source>The Sphere of the Earth</source>
         <translation>La Esfera Terrestre</translation>
     </message>
     <message>
-        <location filename="ui_soe.py" line="359"/>
+        <location filename="ui_soe.py" line="315"/>
         <source>What is a map?</source>
         <translation>¿Qué es un mapa?</translation>
     </message>
     <message>
-        <location filename="ui_soe.py" line="360"/>
-        <source>Plate Carr&#xc3;&#xa9;e</source>
+        <location filename="ui_soe.py" line="316"/>
+        <source>Plate CarrÃ©e</source>
         <translation>Plate Carrée</translation>
     </message>
     <message>
-        <location filename="ui_soe.py" line="361"/>
+        <location filename="ui_soe.py" line="317"/>
         <source>Mercator</source>
         <translation>Mercator</translation>
     </message>
     <message>
-        <location filename="ui_soe.py" line="362"/>
+        <location filename="ui_soe.py" line="318"/>
         <source>Gall-Peters</source>
         <translation>Gall-Peters</translation>
     </message>
     <message>
-        <location filename="ui_soe.py" line="363"/>
+        <location filename="ui_soe.py" line="319"/>
         <source>Azimuthal Equidistant</source>
         <translation>Azimutal Equidistante</translation>
     </message>
     <message>
-        <location filename="ui_soe.py" line="364"/>
+        <location filename="ui_soe.py" line="320"/>
         <source>Gnomonic</source>
         <translation>Gnomónica</translation>
     </message>
     <message>
-        <location filename="ui_soe.py" line="365"/>
+        <location filename="ui_soe.py" line="321"/>
         <source>Mollweide</source>
         <translation>Mollweide</translation>
     </message>
     <message>
-        <location filename="ui_soe.py" line="382"/>
+        <location filename="ui_soe.py" line="322"/>
         <source>About</source>
         <translation>Acerca de</translation>
-    </message>
-    <message>
-        <location filename="ui_soe.py" line="366"/>
-        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
-&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
-p, li { white-space: pre-wrap; }
-&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:200;&quot;&gt;The Sphere of the Earth.&lt;/span&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:200;&quot;&gt;Version 1.1.0 &lt;/span&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:12pt; font-weight:200;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:12pt; font-weight:200;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:200;&quot;&gt;Author: &lt;/span&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:200;&quot;&gt;Daniel Ramos &lt;/span&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:200;&quot;&gt;daniel.ramos@imaginary.org&lt;/span&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:12pt; font-weight:200;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:12pt; font-weight:200;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:200;&quot;&gt;www.mmaca.cat&lt;/span&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:200;&quot;&gt;www.imaginary.org&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation></translation>
     </message>
     <message>
         <location filename="ui_soe.py" line="353"/>
@@ -74,39 +55,77 @@ p, li { white-space: pre-wrap; }
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
 &lt;p align=&quot;center&quot; style=&quot; margin-top:18px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:28pt; font-weight:600;&quot;&gt;The Sphere of the Earth&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+        <translation type="obsolete">&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
 &lt;p align=&quot;center&quot; style=&quot; margin-top:18px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:28pt; font-weight:600;&quot;&gt;La Esfera Terrestre&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="ui_soe.py" line="358"/>
+        <location filename="ui_soe.py" line="314"/>
         <source>Info</source>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_soe.py" line="309"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:28pt; font-weight:600;&quot;&gt;The Sphere of the Earth&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:28pt; font-weight:600;&quot;&gt;La Esfera Terrestre&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
 <context>
     <name>maptab</name>
     <message>
-        <location filename="maptab.py" line="86"/>
+        <location filename="maptab.py" line="162"/>
         <source>Clear</source>
         <translation>Borrar</translation>
     </message>
     <message>
-        <location filename="maptab.py" line="87"/>
+        <location filename="maptab.py" line="160"/>
         <source>Coordinates:</source>
         <translation>Coordenadas:</translation>
     </message>
     <message>
-        <location filename="maptab.py" line="85"/>
-        <source>r=</source>
-        <translation type="unfinished"></translation>
+        <location filename="maptab.py" line="151"/>
+        <source>Geodesic</source>
+        <translation>Geodésica</translation>
     </message>
     <message>
-        <location filename="maptab.py" line="88"/>
-        <source> </source>
-        <translation type="unfinished"></translation>
+        <location filename="maptab.py" line="157"/>
+        <source>Extend</source>
+        <translation>Extender</translation>
+    </message>
+    <message>
+        <location filename="maptab.py" line="153"/>
+        <source>Distance:</source>
+        <translation>Distancia:</translation>
+    </message>
+    <message>
+        <location filename="maptab.py" line="154"/>
+        <source>Unit:</source>
+        <translation>Unidad:</translation>
+    </message>
+    <message>
+        <location filename="maptab.py" line="156"/>
+        <source>Loxodrome</source>
+        <translation>Loxodroma</translation>
+    </message>
+    <message>
+        <location filename="maptab.py" line="159"/>
+        <source>Tissot&apos;s Indicatrix</source>
+        <translation>Indicatriz de Tissot</translation>
+    </message>
+    <message>
+        <location filename="maptab.py" line="161"/>
+        <source>Radius:</source>
+        <translation>Radio:</translation>
     </message>
 </context>
 </TS>
--- Program/soe_fr.ts
+++ Program/soe_fr.ts
@@ -1,9 +1,10 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="fr_FR" sourcelanguage="">
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
 <context>
     <name>Form</name>
     <message>
-        <location filename="ui_soe.py" line="352"/>
+        <location filename="ui_soe.py" line="308"/>
         <source>The Sphere of the Earth</source>
         <translation>La Sphère de la Terre</translation>
     </message>
@@ -14,87 +15,75 @@
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
 &lt;p align=&quot;center&quot; style=&quot; margin-top:18px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:28pt; font-weight:600;&quot;&gt;The Sphere of the Earth&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+        <translation type="obsolete">&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
 &lt;p align=&quot;center&quot; style=&quot; margin-top:18px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:28pt; font-weight:600;&quot;&gt;La Sphère de la Terre&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="ui_soe.py" line="358"/>
+        <location filename="ui_soe.py" line="314"/>
         <source>Info</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui_soe.py" line="359"/>
+        <location filename="ui_soe.py" line="315"/>
         <source>What is a map?</source>
         <translation>Qu&apos;est-ce qu&apos;une carte?</translation>
     </message>
     <message>
-        <location filename="ui_soe.py" line="360"/>
-        <source>Plate Carr&#xc3;&#xa9;e</source>
-        <translation type="unfinished"></translation>
+        <location filename="ui_soe.py" line="316"/>
+        <source>Plate CarrÃ©e</source>
+        <translation>Plate Carrée</translation>
     </message>
     <message>
-        <location filename="ui_soe.py" line="361"/>
+        <location filename="ui_soe.py" line="317"/>
         <source>Mercator</source>
-        <translation type="unfinished"></translation>
+        <translation>Mercator</translation>
     </message>
     <message>
-        <location filename="ui_soe.py" line="362"/>
+        <location filename="ui_soe.py" line="318"/>
         <source>Gall-Peters</source>
-        <translation type="unfinished"></translation>
+        <translation>Gall-Peters</translation>
     </message>
     <message>
-        <location filename="ui_soe.py" line="363"/>
+        <location filename="ui_soe.py" line="319"/>
         <source>Azimuthal Equidistant</source>
         <translation>Azimutale équidistante</translation>
     </message>
     <message>
-        <location filename="ui_soe.py" line="364"/>
+        <location filename="ui_soe.py" line="320"/>
         <source>Gnomonic</source>
         <translation>Gnomonique</translation>
     </message>
     <message>
-        <location filename="ui_soe.py" line="365"/>
+        <location filename="ui_soe.py" line="321"/>
         <source>Mollweide</source>
-        <translation type="unfinished"></translation>
+        <translation>Mollweide</translation>
     </message>
     <message>
-        <location filename="ui_soe.py" line="382"/>
+        <location filename="ui_soe.py" line="322"/>
         <source>About</source>
         <translation>À propos</translation>
     </message>
     <message>
-        <location filename="ui_soe.py" line="366"/>
+        <location filename="ui_soe.py" line="309"/>
         <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:200;&quot;&gt;The Sphere of the Earth.&lt;/span&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:200;&quot;&gt;Version 1.1.0 &lt;/span&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:12pt; font-weight:200;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:12pt; font-weight:200;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:200;&quot;&gt;Author: &lt;/span&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:200;&quot;&gt;Daniel Ramos &lt;/span&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:200;&quot;&gt;daniel.ramos@imaginary.org&lt;/span&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:12pt; font-weight:200;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:12pt; font-weight:200;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:200;&quot;&gt;www.mmaca.cat&lt;/span&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:200;&quot;&gt;www.imaginary.org&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+&lt;p align=&quot;center&quot; style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:28pt; font-weight:600;&quot;&gt;The Sphere of the Earth&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:28pt; font-weight:600;&quot;&gt;La Sphère de la Terre&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
 <context>
     <name>maptab</name>
     <message>
-        <location filename="maptab.py" line="85"/>
-        <source>r=</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="maptab.py" line="86"/>
+        <location filename="maptab.py" line="162"/>
         <source>Clear</source>
         <translation>Effacer</translation>
     </message>
@@ -104,14 +93,44 @@ p, li { white-space: pre-wrap; }
         <translation type="obsolete">Coordonnées:</translation>
     </message>
     <message>
-        <location filename="maptab.py" line="87"/>
+        <location filename="maptab.py" line="160"/>
         <source>Coordinates:</source>
         <translation>Coordonnées:</translation>
     </message>
     <message>
-        <location filename="maptab.py" line="88"/>
-        <source> </source>
-        <translation type="unfinished"></translation>
+        <location filename="maptab.py" line="151"/>
+        <source>Geodesic</source>
+        <translation>Géodésique</translation>
+    </message>
+    <message>
+        <location filename="maptab.py" line="157"/>
+        <source>Extend</source>
+        <translation>Étendre</translation>
+    </message>
+    <message>
+        <location filename="maptab.py" line="153"/>
+        <source>Distance:</source>
+        <translation>Distance:</translation>
+    </message>
+    <message>
+        <location filename="maptab.py" line="154"/>
+        <source>Unit:</source>
+        <translation>Unité:</translation>
+    </message>
+    <message>
+        <location filename="maptab.py" line="156"/>
+        <source>Loxodrome</source>
+        <translation type="unfinished">Loxodromie</translation>
+    </message>
+    <message>
+        <location filename="maptab.py" line="159"/>
+        <source>Tissot&apos;s Indicatrix</source>
+        <translation>Indicatrice de Tissot</translation>
+    </message>
+    <message>
+        <location filename="maptab.py" line="161"/>
+        <source>Radius:</source>
+        <translation>Rayon:</translation>
     </message>
 </context>
 </TS>
